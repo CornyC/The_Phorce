f0959e9dd319f99ecc3f5bbb3e751ba77382b5c7
CornyC <kaesesahnetorte@googlemail.com>
2026-01-22 11:24:53 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='The_Phorce',
+      version='0.1',
+      description='Flexible Force Matching for Parametrization',
+      author='Viktoria Korn',
+      author_email='viktoria.korn@simtech.uni-stuttgart.de',
+      license='MIT',
+      packages=['The_Phorce'],
+      zip_safe=True,
+      python_requires=">=3.8",
+      install_requires=["numpy>=1.23.5","openmm>=8.0.0","mdanalysis>=2.4.3","scipy>=1.9.3"])
